61e459fca9e1de2fc92f6337ccc9d25591845a27
RodrigoGR <rodrigo.gomez@vasslatam.com>
2019-10-29 11:57:45 -0500

    merge

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { InitialComponent } from './Components/initial/initial.component';
+import { ErrorComponent } from './Components/error/error.component';
 
 const routes: Routes = [
-  {path:'', component:InitialComponent}
+  {path:'bono-prepago-home/:parametro', component:InitialComponent},
+  {path:'notFound', component:ErrorComponent},
+  {path: '**', redirectTo:'notFound', pathMatch: 'full'}
 ];
 
 @NgModule({
