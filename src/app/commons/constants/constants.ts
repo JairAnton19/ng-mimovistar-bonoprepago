6550e27112e2be8fd9ddf7c1e2930c4444162f6a
jaime264 <jaime264@gmail.com>
2019-12-09 12:43:43 -0500

    m

--- src/app/commons/constants/constants.ts
+++ src/app/commons/constants/constants.ts
@@ -1,9 +1,9 @@
 const BASE_URL = 'https://aks-mimovistar-ingress-dev.eastus2.cloudapp.azure.com';
 
 const CONSTANTS = {
-    endPointBonosList: 'demo/v1/bonoprepago/v1/getPromotionsByTime',
-    endPointBonosHome: 'demo/v1/bonoprepago/v1/getAvailablePromotions',
-    endPointCanjearBono: 'demo/v1/bonoprepago/v1/canjearBono/'
+    endPointBonosList: 'bonoprepago/v1/getPromotionsByTime',
+    endPointBonosHome: 'bonoprepago/v1/getAvailablePromotions',
+    endPointCanjearBono: 'bonoprepago/v1/canjearBono/'
 };
 
 
