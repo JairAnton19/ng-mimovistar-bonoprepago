1e60377a30685a7a8142c584f795364e5e87484a
Jose Cordova <jose.cordova@vasslatam.com>
2020-01-17 10:41:38 -0500

    change aks cert

--- src/app/commons/constants/constants.ts
+++ src/app/commons/constants/constants.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://aks-mimovistar-ingress-dev.eastus2.cloudapp.azure.com/';
+const BASE_URL = 'https://aks-mimovistar-ingress-cert.eastus2.cloudapp.azure.com/bonoprepago/';
 
 const CONSTANTS = {
     endPointBonosList: 'bonoprepago/v1/getPromotionsByTime',
