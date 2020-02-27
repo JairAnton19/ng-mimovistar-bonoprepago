0bfe8c678309bf5bbe03613c797dc1324c7a6079
Jose Cordova <jose.cordova@vasslatam.com>
2020-02-26 22:58:47 -0500

    change config

--- src/app/commons/constants/constants.ts
+++ src/app/commons/constants/constants.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://aks-mimovistar-ingress-cert.eastus2.cloudapp.azure.com';
+const BASE_URL = 'https://aks-mimovistar-ingress-prod.eastus2.cloudapp.azure.com';
 
 const CONSTANTS = {
     endPointBonosList: 'bonoprepago/v1/getPromotionsByTime',
