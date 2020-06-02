66043b253a0e8730ebb8092f10bb120c242806ba
Jair Anton <jair.anton97@gmail.com>
2020-06-02 12:42:15 -0500

    validate email

--- src/app/bussiness/fidelizacion/bono-inital/bono-inital.component.ts
+++ src/app/bussiness/fidelizacion/bono-inital/bono-inital.component.ts
@@ -8,19 +8,56 @@ import { Router } from '@angular/router';
 })
 export class BonoInitalComponent implements OnInit {
   platform = null;
-  constructor(private route: Router) { }
+  email: string;
+  errorMessage = false;
+  messageError: string = null;
+
+  constructor(private route: Router) {
+
+  }
 
   ngOnInit() {
   }
+
+  public canjearBono(email: string){
+    if(this.validateEmail(email)){
+      this.errorMessage = false;
+      // falta llamar al servicio
+      this.route.navigate(['/bono-okm'])
+    }
+    else {
+      this.errorMessage = true;
+    }
+
+  }
+
   public backHome() {
     const url = sessionStorage.getItem('urlCallBack');
     window.location.href = url;
   }
+
   public buttonClases() {
     return ['tdp-button', this.platform === 'ios' ? 'fontIos' : 'fontAndroid'];
   }
   public linkRouter(ruta: string) {
     this.route.navigate([ruta]);
+    return true;
+  }
+
+  public validateEmail(email: string):boolean {
+    'use strict';
+    var emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if(email === ''){
+      this.messageError = 'Ingresa tu correo electrónico.';
+      return false;
+    }
+
+    if(!email.match(emailRegex)){
+      this.messageError = 'El correo electrónico no es correcto.';
+      return false;
+    }
+
     return true;
   }
 
--- src/app/bussiness/fidelizacion/bono-terms/bono-terms.component.ts
+++ src/app/bussiness/fidelizacion/bono-terms/bono-terms.component.ts
@@ -1,16 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-bono-terms',
   templateUrl: './bono-terms.component.html',
   styleUrls: ['./bono-terms.component.scss']
 })
+
 export class BonoTermsComponent implements OnInit {
   platform = null;
-  constructor() { }
+  constructor(private _location: Location) { }
 
   ngOnInit() {
   }
+
+  public backClicked() {
+    this._location.back();
+  }
+
   public backHome() {
     const url = sessionStorage.getItem('urlCallBack');
     window.location.href = url;
