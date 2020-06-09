7e52ae784bf2b868f1c36304ae1d48d65cdaae06
kquispeg <katherine.quispe@vasslatam.com>
2020-06-09 18:00:57 -0500

    Modificación de texto no-bono y bono-end

--- src/app/bussiness/bono/initial/initial.component.ts
+++ src/app/bussiness/bono/initial/initial.component.ts
@@ -277,7 +277,7 @@ export class InitialComponent implements OnInit {
       /*await this.globalService.globlalPost(`${CONSTANTS.endPointBonosHome}`, body).subscribe(
         async (response: any) => {*/
           const response = {
-            responseCode:"0",
+            responseCode:"1209",
             responseMessage:"Transacción realizada con Éxito",
             responseData:{
                phone:"eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJOb3Z1bSBCb25vIFByZXBhZ28iLCJpYXQiOjE1OTExMjIxNTYsInN1YiI6IkVuY3J5cHRpbmcgZGF0YSIsImlzcyI6Ik5vdnVtIFdlYiBJZCIsInBob25lIjoiOTIwNzk1MzM2IiwiZXhwIjoxNTkxMTI1NzU2fQ.Qtt8_WnWMrHeoq_hJqazpIIUlAex1ITlcVcONy4qLus",
