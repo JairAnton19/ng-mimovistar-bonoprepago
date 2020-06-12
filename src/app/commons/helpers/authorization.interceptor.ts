549e292b742c931f1b0a04ecdb23352593cc6f49
kquispeg <katherine.quispe@vasslatam.com>
2020-06-12 17:56:57 -0500

    cambio en authorization

--- src/app/commons/helpers/authorization.interceptor.ts
+++ src/app/commons/helpers/authorization.interceptor.ts
@@ -14,7 +14,7 @@ export class AuthorizationInterceptor implements HttpInterceptor {
 
   dev:string = 'bWltb3Zpc3RhcjpnR3V5Vzd2WFJnVmV1THBz' //'bWltb3Zpc3RhcjpnR3V5Vzd2WFJnVmV1THBz'
   cert:string= 'bWltb3Zpc3RhcnRlc3Q6UTQ5Y2tZUlZkS2NFWlk2SA==' //'bWltb3Zpc3RhcjpnR3V5Vzd2WFJnVmV1THBz' 
-  prod: string= 'bWltb3Zpc3RhcnByb2Q6SEJQQmZiQjl6QVZNRVo0Sg==' //'bWltb3Zpc3RhcjpnR3V5Vzd2WFJnVmV1THBz'
+  prod: string= 'bWltb3Zpc3RhcjpnR3V5Vzd2WFJnVmV1THBz' //'bWltb3Zpc3RhcnByb2Q6SEJQQmZiQjl6QVZNRVo0Sg=='
 
   subscriptionDev: string = '2dc510c8323c494c843d86bb74f3d07a';
   subscriptionCert: string = '010682286a2f45ebae76575c637b6b1f';
@@ -23,7 +23,6 @@ export class AuthorizationInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     request = request.clone({
       setHeaders: {
-  cert:string= 'bWltb3Zpc3RhcnRlc3Q6UTQ5Y2tZUlZkS2NFWlk2SA==' //'bWltb3Zpc3RhcjpnR3V5Vzd2WFJnVmV1THBz' 
         'Authorization': `Basic ${this.cert}`,
         'Content-Type': 'application/json',
         'Ocp-Apim-Subscription-Key': `${this.subscriptionCert}`
